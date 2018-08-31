84b31f04a3d97c59e146ffdb3581771baaed6ac0
Gilmar Pimenta <grafos@outlook.com.br>
2018-08-31 11:34:24 -0300

    Gravando arquivos de estudos web

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../template/Main'
+
+
+export default props =>
+    <Main icon="home" title="Inicio"
+    subtitle="projeto Signus React">
+    <div className="display-4">Bem Vindos !</div>
+    <hr/>
+    <p className="mb-0">
+        Espaço para implementação de sistema de cadastro
+    </p>
+
+</Main>
---
+++ signus/config/Migrations/20180815142205_Initial.php
@@ -0,0 +1,404 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    public function up()
+    {
+
+        $this->table('acos')
+            ->addColumn('parent_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('model', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('foreign_key', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('alias', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('lft', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('rght', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'lft',
+                    'rght',
+                ]
+            )
+            ->addIndex(
+                [
+                    'alias',
+                ]
+            )
+            ->create();
+
+        $this->table('aros')
+            ->addColumn('parent_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('model', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('foreign_key', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('alias', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('lft', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('rght', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'lft',
+                    'rght',
+                ]
+            )
+            ->addIndex(
+                [
+                    'alias',
+                ]
+            )
+            ->create();
+
+        $this->table('aros_acos')
+            ->addColumn('aro_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('aco_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('_create', 'string', [
+                'default' => '0',
+                'limit' => 2,
+                'null' => false,
+            ])
+            ->addColumn('_read', 'string', [
+                'default' => '0',
+                'limit' => 2,
+                'null' => false,
+            ])
+            ->addColumn('_update', 'string', [
+                'default' => '0',
+                'limit' => 2,
+                'null' => false,
+            ])
+            ->addColumn('_delete', 'string', [
+                'default' => '0',
+                'limit' => 2,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'aro_id',
+                    'aco_id',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'aco_id',
+                ]
+            )
+            ->create();
+
+        $this->table('articles')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 250,
+                'null' => true,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('url', 'string', [
+                'default' => null,
+                'limit' => 250,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('categories')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 45,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('categories_images')
+            ->addColumn('category_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addColumn('image_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'category_id',
+                ]
+            )
+            ->addIndex(
+                [
+                    'image_id',
+                ]
+            )
+            ->create();
+
+        $this->table('comments')
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 45,
+                'null' => true,
+            ])
+            ->addColumn('email', 'string', [
+                'default' => null,
+                'limit' => 250,
+                'null' => true,
+            ])
+            ->addColumn('comment', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('url', 'string', [
+                'default' => null,
+                'limit' => 250,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('groups')
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('images')
+            ->addColumn('url', 'string', [
+                'default' => null,
+                'limit' => 500,
+                'null' => false,
+            ])
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 500,
+                'null' => true,
+            ])
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('messages')
+            ->addColumn('sender', 'string', [
+                'default' => null,
+                'limit' => 45,
+                'null' => true,
+            ])
+            ->addColumn('receiver', 'string', [
+                'default' => null,
+                'limit' => 45,
+                'null' => true,
+            ])
+            ->addColumn('subject', 'string', [
+                'default' => null,
+                'limit' => 250,
+                'null' => true,
+            ])
+            ->addColumn('message', 'text', [
+                'default' => null,
+                'limit' => 4294967295,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('pages')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 250,
+                'null' => true,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('url', 'string', [
+                'default' => null,
+                'limit' => 250,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('plans')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 45,
+                'null' => true,
+            ])
+            ->addColumn('value', 'float', [
+                'default' => null,
+                'null' => true,
+                'precision' => 11,
+                'scale' => 2,
+            ])
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('articles', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('users')
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => true,
+            ])
+            ->addColumn('group_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('plan_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('categories_images')
+            ->addForeignKey(
+                'category_id',
+                'categories',
+                'id',
+                [
+                    'update' => 'NO_ACTION',
+                    'delete' => 'NO_ACTION'
+                ]
+            )
+            ->addForeignKey(
+                'image_id',
+                'images',
+                'id',
+                [
+                    'update' => 'NO_ACTION',
+                    'delete' => 'NO_ACTION'
+                ]
+            )
+            ->update();
+    }
+
+    public function down()
+    {
+        $this->table('categories_images')
+            ->dropForeignKey(
+                'category_id'
+            )
+            ->dropForeignKey(
+                'image_id'
+            )->save();
+
+        $this->table('acos')->drop()->save();
+        $this->table('aros')->drop()->save();
+        $this->table('aros_acos')->drop()->save();
+        $this->table('articles')->drop()->save();
+        $this->table('categories')->drop()->save();
+        $this->table('categories_images')->drop()->save();
+        $this->table('comments')->drop()->save();
+        $this->table('groups')->drop()->save();
+        $this->table('images')->drop()->save();
+        $this->table('messages')->drop()->save();
+        $this->table('pages')->drop()->save();
+        $this->table('plans')->drop()->save();
+        $this->table('users')->drop()->save();
+    }
+}
